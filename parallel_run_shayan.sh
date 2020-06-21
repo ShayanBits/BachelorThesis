2296f0dcf3cffd7667a7348d19f52def12f7e1fb
Shayan <Shayan.shsh@yahoo.com>
2020-06-21 23:45:52 +0200

    * fix dimension

--- parallel_run_shayan.sh
+++ parallel_run_shayan.sh
@@ -26,7 +26,7 @@ for d in "${dims[@]}";do
         for neg in "${negs[@]}";do
           for loss in "${loss_func[@]}";do
 #          change next line to actual command before running on server
-            command="python3 $CODE_PATH/run.py --do_grid --cuda --do_test --data_path $DATA_PATH --model $model -dim $d --negative_sample_size $neg --batch_size $b --gamma $g --adversarial_temperature $temperature --negative_adversarial_sampling -lr $lr --max_steps $max_steps -save $SAVE_PATH -de --loss $loss"
+            command="python3 $CODE_PATH/run.py --do_grid --cuda --do_test --data_path $DATA_PATH --model $model -d $d --negative_sample_size $neg --batch_size $b --gamma $g --adversarial_temperature $temperature --negative_adversarial_sampling -lr $lr --max_steps $max_steps -save $SAVE_PATH -de --loss $loss"
             python3 $CODE_PATH/run.py --do_grid --cuda --do_test --data_path $DATA_PATH --model $model -dim $d --negative_sample_size $neg --batch_size $b --gamma $g --adversarial_temperature $temperature --negative_adversarial_sampling -lr $lr --max_steps $max_steps -save $SAVE_PATH -de --loss $loss &
             echo  $command
 done
