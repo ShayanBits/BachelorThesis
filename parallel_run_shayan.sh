2ecd0ab9efee15c8fde36652d45cb46e338e58b0
Shayan <Shayan.shsh@yahoo.com>
2020-06-21 23:40:38 +0200

    * fix codes path

--- parallel_run_shayan.sh
+++ parallel_run_shayan.sh
@@ -12,6 +12,7 @@ train_with_groundings="false"
 plot="false"
 max_steps=400000
 
+CODE_PATH="codes"
 DATA_PATH="data/FB15k"
 SAVE_PATH="models/$model/$dataset"
 loss_func=("margin_ranking")
@@ -25,8 +26,8 @@ for d in "${dims[@]}";do
         for neg in "${negs[@]}";do
           for loss in "${loss_func[@]}";do
 #          change next line to actual command before running on server
-            command="python3 run.py --do_grid --cuda --do_test --data_path $DATA_PATH --model $model -dim $d --negative_sample_size $neg --batch_size $b --gamma $g --adversarial_temperature $temperature --negative_adversarial_sampling -lr $lr --max_steps $max_steps -save $SAVE_PATH -de --loss $loss"
-            python3 run.py --do_grid --cuda --do_test --data_path $DATA_PATH --model $model -dim $d --negative_sample_size $neg --batch_size $b --gamma $g --adversarial_temperature $temperature --negative_adversarial_sampling -lr $lr --max_steps $max_steps -save $SAVE_PATH -de --loss $loss &
+            command="python3 $CODE_PATH/run.py --do_grid --cuda --do_test --data_path $DATA_PATH --model $model -dim $d --negative_sample_size $neg --batch_size $b --gamma $g --adversarial_temperature $temperature --negative_adversarial_sampling -lr $lr --max_steps $max_steps -save $SAVE_PATH -de --loss $loss"
+            python3 $CODE_PATH/run.py --do_grid --cuda --do_test --data_path $DATA_PATH --model $model -dim $d --negative_sample_size $neg --batch_size $b --gamma $g --adversarial_temperature $temperature --negative_adversarial_sampling -lr $lr --max_steps $max_steps -save $SAVE_PATH -de --loss $loss &
             echo  $command
 done
 done
