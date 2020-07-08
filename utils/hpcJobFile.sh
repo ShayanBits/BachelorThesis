3feaba266d14f0a2fd92faa3ae4696b347a7ace5
Shayan <Shayan.shsh@yahoo.com>
2020-07-08 22:14:16 +0200

    jobfile 1

---
+++ utils/hpcJobFile.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#Submit this script with: sbatch thefilename
+
+#SBATCH --time=1:00:00   # walltime
+#SBATCH --nodes=1   # number of nodes
+#SBATCH --gres=gpu:4
+#SBATCH --ntasks=1      # limit to one node
+#SBATCH --cpus-per-task=4  # number of processor cores (i.e. threads)
+#SBATCH --partition=gpu2
+#SBATCH --mem-per-cpu=2500M   # memory per CPU core
+#SBATCH -J "test-shayan"   # job name
+#SBATCH --mail-user=shayan.shahpasand@mailbox.tu-dresden.de   # email address
+#SBATCH --mail-type=BEGIN,END,FAIL,REQUEUE,TIME_LIMIT,TIME_LIMIT_90
+#SBATCH -A p_ml_nimi
+
+
+module load PythonAnaconda/3.7
+module load PyTorch
+#module scikit-learn
+
+# Set the max number of threads to use for programs using OpenMP. Should be <= ppn. Does nothing if the program doesn't use OpenMP.
+export OMP_NUM_THREADS=$SLURM_CPUS_ON_NODE
+OUTFILE="shayan-test-output"
+/home/shsh829c/DIR/scratch/shsh829c-KGE_Pattern_scratch/KGE_Pattern/parallel_run_shayan.sh  > "$OUTFILE"
+
+exit 0
