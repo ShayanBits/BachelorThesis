cbe04fc43291505c9633f9171a4b19356936afc5
Shayan <Shayan.shsh@yahoo.com>
2020-08-02 21:35:27 +0200

    limit on to two gpus

--- parallel_run_shayan.sh
+++ parallel_run_shayan.sh
@@ -30,7 +30,7 @@ for d in "${dims[@]}";do
           for loss in "${loss_func[@]}";do
             executed_flag="false"
             while [ $executed_flag != "true" ];do
-            for cpu_number in {0..2};do
+            for cpu_number in {0..1};do
                available_mem=$(nvidia-smi --query-gpu=memory.free --format=csv -i ${cpu_number})
 #               extract the integer value in MB
                available_mem=${available_mem//[^0-9]/}
