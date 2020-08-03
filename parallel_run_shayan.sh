81e11289f30b1839df66625c47e130b4dccca8ed
Shayan <Shayan.shsh@yahoo.com>
2020-08-03 22:26:19 +0200

    adjusted path to hpcs

--- parallel_run_shayan.sh
+++ parallel_run_shayan.sh
@@ -12,10 +12,11 @@ train_with_groundings="false"
 plot="false"
 max_steps=400000
 
+BASE_PATH="/home/shsh829c/DIR/scratch/shsh829c-KGE_Pattern_scratch/KGE_Pattern/"
 CODE_PATH="codes"
 DATA_PATH="data/FB15k"
-loss_func=("rotate")
-SAVE_PATH="models/$model/$loss_func/$dataset"
+LOSS_FUNC=("rotate")
+SAVE_PATH="models/$model/$LOSS_FUNC/$dataset"
 
 executed_flag="false"
 
@@ -26,7 +27,7 @@ for d in "${dims[@]}";do
     for lr in "${lrs[@]}";do
       for b in "${batch_sizes[@]}";do
         for neg in "${negs[@]}";do
-          for loss in "${loss_func[@]}";do
+          for loss in "${LOSS_FUNC[@]}";do
             executed_flag="false"
             while [ $executed_flag != "true" ];do
             for cpu_number in {0..3};do
@@ -36,8 +37,8 @@ for d in "${dims[@]}";do
 #               echo "available mem is: $available_mem"
                if [[ ${available_mem} -gt 1980 ]];then
                    echo "free memory of GPU $cpu_number: $available_mem"
-                   command="CUDA_VISIBLE_DEVICES=$cpu_number python3 $CODE_PATH/run.py --do_grid --cuda --do_test --data_path $DATA_PATH --model $model -d $d --negative_sample_size $neg --batch_size $b --gamma $g --adversarial_temperature $temperature --negative_adversarial_sampling -lr $lr --max_steps $max_steps -save $SAVE_PATH -de --loss $loss"
-                   CUDA_VISIBLE_DEVICES=$cpu_number python3 $CODE_PATH/run.py --do_grid --cuda --do_test --data_path $DATA_PATH --model $model -d $d --negative_sample_size $neg --batch_size $b --gamma $g --adversarial_temperature $temperature --negative_adversarial_sampling -lr $lr --max_steps $max_steps -save $SAVE_PATH -de --loss $loss &
+                   command="CUDA_VISIBLE_DEVICES=$cpu_number python3 $BASE_PATH/$CODE_PATH/run.py --do_grid --cuda --do_test --data_path $BASE_PATH/$DATA_PATH --model $model -d $d --negative_sample_size $neg --batch_size $b --gamma $g --adversarial_temperature $temperature --negative_adversarial_sampling -lr $lr --max_steps $max_steps -save $BASE_PATH/$SAVE_PATH -de --loss $loss"
+                   CUDA_VISIBLE_DEVICES=$cpu_number python3 $BASE_PATH/$CODE_PATH/run.py --do_grid --cuda --do_test --data_path $BASE_PATH/$DATA_PATH --model $model -d $d --negative_sample_size $neg --batch_size $b --gamma $g --adversarial_temperature $temperature --negative_adversarial_sampling -lr $lr --max_steps $max_steps -save $BASE_PATH/$SAVE_PATH -de --loss $loss &
                    echo  "following command is executed"
                    echo  $command
                    executed_flag="true"
