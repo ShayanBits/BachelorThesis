533288e640fee6dcdce78709cd158ad53db3a834
Shayan <Shayan.shsh@yahoo.com>
2020-08-07 12:05:27 +0200

    updated parallel script path in slurm file 2

--- utils/hpcJobFile.sh
+++ utils/hpcJobFile.sh
@@ -23,6 +23,6 @@ OUTFILE="shayan-test-output.log"
 
 
 echo $PWD
-/scratch/ws/1/shsh829c-KGE/KGE_Pattern/utils/parallel_run_shayan.sh  > "$OUTFILE"
+/scratch/ws/1/shsh829c-KGE/KGE_Pattern/parallel_run_shayan.sh  > "$OUTFILE"
 
 exit 0
