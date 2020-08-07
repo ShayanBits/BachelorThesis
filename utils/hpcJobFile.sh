845111fdb501792df1f01ee153b244081b316f8a
Shayan <Shayan.shsh@yahoo.com>
2020-08-08 01:34:48 +0200

    lower memory per cpu

--- utils/hpcJobFile.sh
+++ utils/hpcJobFile.sh
@@ -3,12 +3,12 @@
 #Submit this script with: sbatch thefilename
 
 #SBATCH --time=1:00:00   # walltime
-#SBATCH --nodes=1   # number of nodes -N
+#SBATCH --nodes=1   # number of nodes
 #SBATCH --gres=gpu:2
-#SBATCH --ntasks=1      # limit to one node --ntasks-per-node
+#SBATCH --ntasks=1      # limit to one node
 #SBATCH --cpus-per-task=1  # number of processor cores (i.e. threads)
 #SBATCH --partition=gpu2
-#SBATCH --mem-per-cpu=10000M   # memory per CPU core
+#SBATCH --mem-per-cpu=8000M   # memory per CPU core
 #SBATCH -J "test-shayan"   # job name
 #SBATCH -o test-shayan-slurm-%j.out
 #SBATCH --mail-user=shayan.shahpasand@mailbox.tu-dresden.de   # email address
