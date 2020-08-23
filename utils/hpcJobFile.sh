a71a1c41cdf75c28047a7d27491aff40eb49e872
Shayan <Shayan.shsh@yahoo.com>
2020-08-23 08:36:10 +0200

    changed jobName and time

--- utils/hpcJobFile.sh
+++ utils/hpcJobFile.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 #Submit this script with: sbatch thefilename
-#SBATCH --time=26:00:00   # walltime
+#SBATCH --time=30:00:00   # walltime
 #SBATCH --nodes=1   # number of nodes
 #SBATCH --gres=gpu:1
 #SBATCH --ntasks=1      # limit to one node
 #SBATCH --cpus-per-task=1  # number of processor cores (i.e. threads)
 #SBATCH --mem-per-cpu=10000M   # memory per CPU core
-#SBATCH -J "fb17k-new-grid"   # job name
-#SBATCH -o fb17k-new-grid-%j.out
+#SBATCH -J "fb17k-new-grid-DistMult"   # job name
+#SBATCH -o fb17k-new-grid-DistMult-%j.out
 #SBATCH --mail-user=shayan.shahpasand@mailbox.tu-dresden.de   # email address
 #SBATCH --mail-type=BEGIN,END,FAIL,REQUEUE,TIME_LIMIT,TIME_LIMIT_90
 #SBATCH -A p_ml_nimi
