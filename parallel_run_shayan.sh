3098b4121ae189129ae6c2e7880a3efe257b6fe6
Shayan <Shayan.shsh@yahoo.com>
2020-09-18 16:29:55 +0200

    round two

--- parallel_run_shayan.sh
+++ parallel_run_shayan.sh
@@ -6,15 +6,15 @@
 #lrs=(0.01 0.05 0.1)
 #batch_sizes=(1024)
 #negs=(10 100 1000)
-dims=(100)
-gamma=(50)
+dims=(1000)
+gamma=(1 10 20 30 40 50)
 temperature=1
-lrs=(0.05)
+lrs=(0.01 0.05 0.1)
 batch_sizes=(512)
 negs=(100)
 #model="TransE"
-models=("TransE")
-#declare -a model=("TransE" "RotatE" "ComplEx" "QuatE" "Dismult")
+#models=("TransE")
+models=("TransE" "RotatE" "ComplEx" "QuatE" "DistMult")
 dataset="wn18"
 train_with_groundings="false"
 plot="false"
@@ -22,8 +22,8 @@ max_steps=400000
 
 CODE_PATH="../codes"
 DATA_PATH="../data/$dataset"
-LOSS_FUNC=("rotate")
-#LOSS_FUNC=("rotate" "margin_ranking" "adaptive_margin")
+#LOSS_FUNC=("rotate")
+LOSS_FUNC=("rotate" "margin_ranking" "adaptive_margin")
 
 executed_flag="false"
 email_address="shayan.shahpasand@mailbox.tu-dresden.de"
@@ -52,7 +52,7 @@ for d in "${dims[@]}";do
 #               if [[ ${available_mem} -gt 1980 ]];then
 #                   echo "free memory of GPU $gpu_number: $available_mem"
 #                   command="CUDA_VISIBLE_DEVICES=$gpu_number python3 $CODE_PATH/run.py --do_grid --cuda --do_test --data_path $DATA_PATH --model $model -d $d --negative_sample_size $neg --batch_size $b --gamma $g --adversarial_temperature $temperature --negative_adversarial_sampling -lr $lr --max_steps $max_steps -save $SAVE_PATH/dim-$d/gamma-$g/learning-rate-$lr/batch-size-$b/negative-sample-size-$neg/ -de --loss $loss"
-                   command="python3 $CODE_PATH/run.py --do_grid --cuda --do_test --data_path $DATA_PATH --model $model -d $d --negative_sample_size $neg --batch_size $b --gamma $g --adversarial_temperature $temperature --negative_adversarial_sampling -lr $lr --max_steps $max_steps -save $COMPLETE_SAVE_PATH -de --loss $loss --init_checkpoint $COMPLETE_SAVE_PATH"
+                   command="python3 $CODE_PATH/run.py --do_grid --cuda --do_test --data_path $DATA_PATH --model $model -d $d --negative_sample_size $neg --batch_size $b --gamma $g --adversarial_temperature $temperature --negative_adversarial_sampling -lr $lr --max_steps $max_steps -save $COMPLETE_SAVE_PATH -de --loss $loss"
                    srunCommand="srun --time=2:00:00 --nodes=1 --gres=gpu:1 --ntasks=1 --cpus-per-task=1 --partition=gpu2-interactive -J $job_name -o "$job_name-slurm-%j.log" --mail-user=$email_address --mail-type="ALL" -A "p_ml_nimi" source /home/sava096c/envs/env01/bin/activate python3 $command"
                    echo  "following command is executed"
                    echo  $command >> utils/commands.txt
--- utils/hpcJobFile.sh
+++ utils/hpcJobFile.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 #Submit this script with: sbatch thefilename
-#SBATCH --time=12:00:00   # walltime
+#SBATCH --time=25:00:00   # walltime
 #SBATCH --nodes=1   # number of nodes
 #SBATCH --gres=gpu:1
 #SBATCH --ntasks=1      # limit to one node
 #SBATCH --cpus-per-task=1  # number of processor cores (i.e. threads)
 #SBATCH --mem-per-cpu=10000M   # memory per CPU core
-#SBATCH -J "wn18-new-grid-last-one"   # job name
-#SBATCH -o wn18-new-grid-last-one%j.out
+#SBATCH -J "wn18-new-grid-round-two"   # job name
+#SBATCH -o wn18-new-grid-round-two%j.out
 #SBATCH --mail-user=shayan.shahpasand@mailbox.tu-dresden.de   # email address
 #SBATCH --mail-type=BEGIN,END,FAIL,REQUEUE,TIME_LIMIT,TIME_LIMIT_90
+#SBATCH --reservation=p_ml_nimi_105
 #SBATCH -A p_ml_nimi
-#SBATCH --array=1-1
+#SBATCH --array=1-270
 
 
 source /home/sava096c/envs/env01/bin/activate
