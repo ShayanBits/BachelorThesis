d8eeb22af2ad75676b1e7eba0c53b16aec00deda
Shayan <Shayan.shsh@yahoo.com>
2020-09-18 12:56:32 +0200

    last 4

--- utils/hpcJobFile.sh
+++ utils/hpcJobFile.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 #Submit this script with: sbatch thefilename
-#SBATCH --time=2:00:00   # walltime
+#SBATCH --time=5:00:00   # walltime
 #SBATCH --nodes=1   # number of nodes
 #SBATCH --gres=gpu:1
 #SBATCH --ntasks=1      # limit to one node
 #SBATCH --cpus-per-task=1  # number of processor cores (i.e. threads)
 #SBATCH --mem-per-cpu=10000M   # memory per CPU core
-#SBATCH -J "wn18rr-new-grid-fillup-checkpoint"   # job name
-#SBATCH -o wn18rr-new-grid-fillup-checkpoint-%j.out
+#SBATCH -J "wn18rr-new-grid-fillup-checkpoint-last4"   # job name
+#SBATCH -o wn18rr-new-grid-fillup-checkpoint-last4%j.out
 #SBATCH --mail-user=shayan.shahpasand@mailbox.tu-dresden.de   # email address
 #SBATCH --mail-type=BEGIN,END,FAIL,REQUEUE,TIME_LIMIT,TIME_LIMIT_90
 #SBATCH -A p_ml_nimi
-#SBATCH --array=1-14
+#SBATCH --array=1-4
 
 
 source /home/sava096c/envs/env01/bin/activate
