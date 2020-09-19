e11757965fc512fc7b9531c2181293f4093660c9
Shayan <Shayan.shsh@yahoo.com>
2020-09-19 22:56:17 +0200

    round two

--- utils/hpcJobFile.sh
+++ utils/hpcJobFile.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 #Submit this script with: sbatch thefilename
-#SBATCH --time=20:00:00   # walltime
+#SBATCH --time=40:00:00   # walltime
 #SBATCH --nodes=1   # number of nodes
 #SBATCH --gres=gpu:1
 #SBATCH --ntasks=1      # limit to one node
 #SBATCH --cpus-per-task=1  # number of processor cores (i.e. threads)
 #SBATCH --mem-per-cpu=10000M   # memory per CPU core
-#SBATCH -J "FB15k-237-new-grid-fillup-checkpoint4-reservation"   # job name
-#SBATCH -o FB15k-237-new-grid-fillup-checkpoint4-reservation-%j.out
+#SBATCH -J "fb15k237-new-grid-round-two"   # job name
+#SBATCH -o fb15k237-new-grid-round-two%j.out
 #SBATCH --mail-user=shayan.shahpasand@mailbox.tu-dresden.de   # email address
 #SBATCH --mail-type=BEGIN,END,FAIL,REQUEUE,TIME_LIMIT,TIME_LIMIT_90
-#SBATCH -A p_ml_nimi
 #SBATCH --reservation=p_ml_nimi_105
-#SBATCH --array=1-11
+#SBATCH -A p_ml_nimi
+#SBATCH --array=1-270
 
 
 source /home/shsh829c/venv/env1/bin/activate
