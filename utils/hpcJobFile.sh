6eeaf76d07fc7b1d5b051af028e728eaca0c6da6
Shayan <Shayan.shsh@yahoo.com>
2020-09-30 20:11:48 +0200

    round 4

--- utils/hpcJobFile.sh
+++ utils/hpcJobFile.sh
@@ -7,13 +7,13 @@
 #SBATCH --ntasks=1      # limit to one node
 #SBATCH --cpus-per-task=4  # number of processor cores (i.e. threads)
 #SBATCH --mem-per-cpu=3875M   # memory per CPU core
-#SBATCH -J "wn18rr-new-grid-round-three"   # job name
-#SBATCH -o wn18rr-new-grid-round-three%j.out
+#SBATCH -J "wn18rr-new-grid-round-four"   # job name
+#SBATCH -o wn18rr-new-grid-round-four%j.out
 #SBATCH --mail-user=shayan.shahpasand@mailbox.tu-dresden.de   # email address
 #SBATCH --mail-type=BEGIN,END,FAIL,REQUEUE,TIME_LIMIT,TIME_LIMIT_90
 #SBATCH --reservation=p_ml_nimi_105
 #SBATCH -A p_ml_nimi
-#SBATCH --array=1-1080
+#SBATCH --array=1-810
 
 
 source /home/shsh829c/venv/env1/bin/activate
