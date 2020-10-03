390c4f0f2947ddd224cb888d5f34c188fddb0b3d
Shayan <Shayan.shsh@yahoo.com>
2020-10-03 19:35:37 +0200

    symmetric test set test

--- parallel_run_shayan.sh
+++ parallel_run_shayan.sh
@@ -1,26 +1,40 @@
 #!/usr/bin/env bash
 
-dims=(50 1000)
-gamma=(1 5 10 20 30 40 50 100)
+#dims=(10 100 1000)
+#gamma=(1 10 20 30 40 50)
+#temperature=1
+#lrs=(0.01 0.05 0.1)
+#batch_sizes=(1024)
+#negs=(10 100 1000)
+dims=(10)
+gamma=(1)
 temperature=1
-lrs=(0.01  0.05  0.1)
-batch_sizes=(512 1024)
-negs=(10 50 100 500 1000)
+lrs=(0.01)
+batch_sizes=(1024)
+#negs=(10 100 1000)
+negs=(10)
+#models=("TransE" "RotatE" "ComplEx" "QuatE" "DistMult")
+models=("TransE")
+#models=("TransE")
 dataset="FB15k"
-model="RotatE"
 train_with_groundings="false"
 plot="false"
 max_steps=400000
 
 CODE_PATH="../codes"
-DATA_PATH="../data/FB15k"
+DATA_PATH="../data/$dataset"
 LOSS_FUNC=("rotate")
-SAVE_PATH="../models/$model/$LOSS_FUNC/$dataset"
+#LOSS_FUNC=("rotate" "margin_ranking" "adaptive_margin")
 
 executed_flag="false"
+email_address="shayan.shahpasand@mailbox.tu-dresden.de"
+job_name="fb15k-complete-checkpoint-last-one"
 
+# Always run the following line in your hpc user before using srun command
+# source /home/sava096c/envs/env01/bin/activate
 echo "starting grid run on all variables"
 
+for model in "${models[@]}";do
 for d in "${dims[@]}";do
   for g in "${gamma[@]}";do
     for lr in "${lrs[@]}";do
@@ -30,15 +44,18 @@ for d in "${dims[@]}";do
             executed_flag="false"
 #            while [ $executed_flag != "true" ];do
             for gpu_number in 0;do
+                SAVE_PATH="../models/$model/$loss/$dataset"
+                COMPLETE_SAVE_PATH="$SAVE_PATH/dim-$d/gamma-$g/learning-rate-$lr/batch-size-$b/negative-sample-size-$neg/"
 #               available_mem=$(nvidia-smi --query-gpu=memory.free --format=csv -i ${gpu_number})
 #               extract the integer value in MB
 #               available_mem=${available_mem//[^0-9]/}
 #               echo "available mem is: $available_mem"
 #               if [[ ${available_mem} -gt 1980 ]];then
-                   echo "free memory of GPU $gpu_number: $available_mem"
+#                   echo "free memory of GPU $gpu_number: $available_mem"
 #                   command="CUDA_VISIBLE_DEVICES=$gpu_number python3 $CODE_PATH/run.py --do_grid --cuda --do_test --data_path $DATA_PATH --model $model -d $d --negative_sample_size $neg --batch_size $b --gamma $g --adversarial_temperature $temperature --negative_adversarial_sampling -lr $lr --max_steps $max_steps -save $SAVE_PATH/dim-$d/gamma-$g/learning-rate-$lr/batch-size-$b/negative-sample-size-$neg/ -de --loss $loss"
-                   command="python3 $CODE_PATH/run.py --do_grid --cuda --do_test --data_path $DATA_PATH --model $model -d $d --negative_sample_size $neg --batch_size $b --gamma $g --adversarial_temperature $temperature --negative_adversarial_sampling -lr $lr --max_steps $max_steps -save $SAVE_PATH/dim-$d/gamma-$g/learning-rate-$lr/batch-size-$b/negative-sample-size-$neg/ -de --loss $loss"
-#                   srun --time=1:00:00 --nodes=1 --gres=gpu:1 --ntasks=1 --cpus-per-task=1 --partition=gpu2 -J "test-shayan" -o "test-shayan-slurm-%j.log" --mail-user="shayan.shahpasand@mailbox.tu-dresden.de" --mail-type="ALL" -A "p_ml_nimi" source /home/shsh829c/venv/env1/bin/activate OUTFILE="shayan-test-output.log"  CUDA_VISIBLE_DEVICES=$cpu_number python3 $CODE_PATH/run.py --do_grid --cuda --do_test --data_path $DATA_PATH --model $model -d $d --negative_sample_size $neg --batch_size $b --gamma $g --adversarial_temperature $temperature --negative_adversarial_sampling -lr $lr --max_steps $max_steps -save $SAVE_PATH -de --loss $loss > "$OUTFILE" &
+#                   command="python3 $CODE_PATH/run.py --do_grid --cuda --do_test --data_path $DATA_PATH --model $model -d $d --negative_sample_size $neg --batch_size $b --gamma $g --adversarial_temperature $temperature --negative_adversarial_sampling -lr $lr --max_steps $max_steps -save $COMPLETE_SAVE_PATH -de --loss $loss"
+                   command="python3 $CODE_PATH/run.py --cuda --do_test --data_path $DATA_PATH --model $model -d $d --negative_sample_size $neg --batch_size $b --gamma $g --adversarial_temperature $temperature --negative_adversarial_sampling -lr $lr --max_steps $max_steps -save $COMPLETE_SAVE_PATH -de --loss $loss --init_checkpoint $COMPLETE_SAVE_PATH"
+#                   srunCommand="srun --time=2:00:00 --nodes=1 --gres=gpu:1 --ntasks=1 --cpus-per-task=1 --partition=gpu2-interactive -J $job_name -o "$job_name-slurm-%j.log" --mail-user=$email_address --mail-type="ALL" -A "p_ml_nimi" source /home/sava096c/envs/env01/bin/activate python3 $command"
                    echo  "following command is executed"
                    echo  $command >> utils/commands.txt
 #                   executed_flag="true"
@@ -47,6 +64,7 @@ for d in "${dims[@]}";do
 #               fi
 #               sleep 5
 #            done
+done
 done
 done
 done
