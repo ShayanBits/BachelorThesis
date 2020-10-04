401b0e4089fe5e418abc69aefa0f17311f7dc8df
Shayan <Shayan.shsh@yahoo.com>
2020-10-04 13:53:29 +0200

    last 551

--- utils/hpcJobFile.sh
+++ utils/hpcJobFile.sh
@@ -13,7 +13,7 @@
 #SBATCH --mail-type=BEGIN,END,FAIL,REQUEUE,TIME_LIMIT,TIME_LIMIT_90
 #SBATCH --reservation=p_ml_nimi_105
 #SBATCH -A p_ml_nimi
-#SBATCH --array=1-810
+#SBATCH --array=1-551
 
 
 source /home/shsh829c/venv/env1/bin/activate
