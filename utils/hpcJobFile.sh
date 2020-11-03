6d2e638afe06fb29617ab1eb054d2b089140b79c
Shayan <Shayan.shsh@yahoo.com>
2020-11-03 23:11:49 +0100

    adjust jobfile and run.py

--- utils/hpcJobFile.sh
+++ utils/hpcJobFile.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 #Submit this script with: sbatch thefilename
-#SBATCH --time=40:00:00   # walltime
+#SBATCH --time=10:00:00   # walltime
 #SBATCH --nodes=1   # number of nodes
 #SBATCH --gres=gpu:1
 #SBATCH --ntasks=1      # limit to one node
 #SBATCH --cpus-per-task=4  # number of processor cores (i.e. threads)
 #SBATCH --mem-per-cpu=3875M   # memory per CPU core
-#SBATCH -J "wn18rr-filling-up-the-blanks"   # job name
-#SBATCH -o wn18rr-filling-up-the-blanks%j.out
+#SBATCH -J "wn18rr-run-sym"   # job name
+#SBATCH -o wn18rr-run-sym-%j.out
 #SBATCH --mail-user=shayan.shahpasand@mailbox.tu-dresden.de   # email address
 #SBATCH --mail-type=BEGIN,END,FAIL,REQUEUE,TIME_LIMIT,TIME_LIMIT_90
-#SBATCH --reservation=p_ml_nimi_105
+#SBATCH --reservation=p_ml_nimi_137
 #SBATCH -A p_ml_nimi
 #SBATCH --array=1-2430
 
