fae705bbd8d00fefde0c4b4b663cd2b42936a560
Shayan <Shayan.shsh@yahoo.com>
2020-11-04 14:13:52 +0100

    run implication

--- utils/hpcJobFile.sh
+++ utils/hpcJobFile.sh
@@ -7,13 +7,13 @@
 #SBATCH --ntasks=1      # limit to one node
 #SBATCH --cpus-per-task=4  # number of processor cores (i.e. threads)
 #SBATCH --mem-per-cpu=3875M   # memory per CPU core
-#SBATCH -J "wn18-run-sym-take2"   # job name
-#SBATCH -o wn18-run-sym-take2-%j.out
+#SBATCH -J "wn18-run-impl"   # job name
+#SBATCH -o wn18-run-impl-%j.out
 #SBATCH --mail-user=shayan.shahpasand@mailbox.tu-dresden.de   # email address
 #SBATCH --mail-type=BEGIN,END,FAIL,REQUEUE,TIME_LIMIT,TIME_LIMIT_90
 #SBATCH --reservation=p_ml_nimi_137
 #SBATCH -A p_ml_nimi
-#SBATCH --array=1-2430%78
+#SBATCH --array=1-2430%80
 
 
 source /home/shsh829c/venv/env1/bin/activate
