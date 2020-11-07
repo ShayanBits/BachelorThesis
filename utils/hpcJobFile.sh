cc56089c429fd378d63ea475fdf9f256d209ac93
Shayan <Shayan.shsh@yahoo.com>
2020-11-07 15:46:29 +0100

    missing 72

--- utils/hpcJobFile.sh
+++ utils/hpcJobFile.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 #Submit this script with: sbatch thefilename
-#SBATCH --time=10:00:00   # walltime
+#SBATCH --time=40:00:00   # walltime
 #SBATCH --nodes=1   # number of nodes
 #SBATCH --gres=gpu:1
 #SBATCH --ntasks=1      # limit to one node
 #SBATCH --cpus-per-task=4  # number of processor cores (i.e. threads)
 #SBATCH --mem-per-cpu=3875M   # memory per CPU core
-#SBATCH -J "wn18-run-1-N"   # job name
-#SBATCH -o wn18-run-1-N-%j.out
+#SBATCH -J "wn18-missing-72"   # job name
+#SBATCH -o wn18-missing-72-%j.out
 #SBATCH --mail-user=shayan.shahpasand@mailbox.tu-dresden.de   # email address
 #SBATCH --mail-type=BEGIN,END,FAIL,REQUEUE,TIME_LIMIT,TIME_LIMIT_90
 #SBATCH --reservation=p_ml_nimi_137
 #SBATCH -A p_ml_nimi
-#SBATCH --array=1-2430%80
+#SBATCH --array=1-72
 
 
 source /home/shsh829c/venv/env1/bin/activate
